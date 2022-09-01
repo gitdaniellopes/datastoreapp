f29e5b9d5b44898458277b6fcfe687b672028b51
Daniel Lopes <danieljfsantos@gmail.com>
2022-09-01 09:42:54 -0300

    add dependencies, repositories and more packages

---
+++ DataStoreApp/app/src/main/java/br/com/datastoreapp/data/source/preferences/Preferences.kt
@@ -0,0 +1,8 @@
+package br.com.datastoreapp.data.source.preferences
+
+interface Preferences {
+
+    suspend fun putDarkThemeValue(key: String, value: Boolean)
+
+    suspend fun getDarkThemeValue(key: String): Boolean
+}
---
+++ DataStoreApp/app/src/main/java/br/com/datastoreapp/domain/repository/Repository.kt
@@ -0,0 +1,8 @@
+package br.com.datastoreapp.domain.repository
+
+interface Repository {
+
+    suspend fun putDarkThemeValue(key: String, value: Boolean)
+
+    suspend fun getDarkThemeValue(key: String): Boolean
+}
